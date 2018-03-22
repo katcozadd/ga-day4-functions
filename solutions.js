87b7410a8ee9413a6f29a10847e1a310f20e08b7
katcozadd <krosecozadd@gmail.com>
2018-03-22 16:57:25 -0600

    Commit 1 - verbal questions

---
+++ solutions.js
@@ -0,0 +1,12 @@
+//Verbal Questions
+//1. What is the difference between a parameter and an argument?
+//When initially writing a function, the parameter is the first set of () that you run into for example:
+//const myFunction = (*this is the parameter*) => {
+//	console.log(lalala);
+//}
+//The argument is the value that you put into the () when calling the function.
+
+//2. Within a function, what is the difference between return and console.log?
+//console.log will only print out what is in the () in the console.
+//Return statements will print out what the function evaluates to.
+
