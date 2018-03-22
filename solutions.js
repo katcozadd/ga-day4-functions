b548e65ee59f5f9631066fc9000f5c6f633a4242
katcozadd <krosecozadd@gmail.com>
2018-03-22 17:24:46 -0600

    Commit 2 - Palindrome

--- solutions.js
+++ solutions.js
@@ -8,5 +8,13 @@
 
 //2. Within a function, what is the difference between return and console.log?
 //console.log will only print out what is in the () in the console.
-//Return statements will print out what the function evaluates to.
+//Return statements will print out what the function evaluates to in the program.
+
+//Palindrome
+//Write a function checkPalindrome that accepts a single argument, a string. The function should return true (Boolean) if the string is a palindrome, false if it is not. Make sure your function will give the correct answer for words with capital letters.
+const checkPalindrome = (word) => {
+		return word === word.split('').reverse('').join('');
+};
+
+console.log(checkPalindrome("Radar"));
 
